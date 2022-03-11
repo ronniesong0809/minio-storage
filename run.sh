3d5c3f8e07421e45908e41f8cee6c5fe8d86e9d8
Ronnie Song <ronniesong0809@gmail.com>
2022-03-10 19:37:01 -0800

    a minor fix

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ wget https://dl.min.io/server/minio/release/linux-amd64/minio
 chmod +x minio
 export MINIO_ACCESS_KEY="$ACCESS_KEY"
 export MINIO_SECRET_KEY="$SECRET_KEY"
-./minio server /mnt/data --address :$PORT
+./minio server /mnt/minio/data --console-address :$PORT
